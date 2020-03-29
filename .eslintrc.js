93f6a6fc86ce07b0825cc032b1955b665da9ca27
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2020-03-29 02:06:08 -0300

    Init

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json"
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["airbnb-typescript"],
+  rules: {
+    "no-console": "off"
+  }
+};
